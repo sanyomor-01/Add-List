97a2fd3242f9ad67e6be6cc03e88a04cb1b3ea77
sanyomor-01 <sanyomormichael@gmail.com>
2024-01-21 06:58:55 +0000

    update list in realtime

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //initialize firebase
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push,onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL : "https://realtime-database-9c889-default-rtdb.firebaseio.com/"
@@ -14,13 +14,29 @@ const inputField = document.getElementById('input-el')
 const addButton = document.getElementById('add-button')
 const shoppingListEl = document.getElementById('shopping-list')
 
-
 function addListItem(item) {
      shoppingListEl.innerHTML += `<li>${item}</li>`
 }
 function clearInputField() {
       inputField.value = ''
 }
+function clearSnapshot() {
+    shoppingListEl.innerHTML =""
+}
+//Updates Item in real time
+onValue(shoppingListInDB, function(snapshot) {
+    clearSnapshot()
+    
+    let shoppingListArray = Object.values(snapshot.val())
+
+    for(let i = 0; i<shoppingListArray.length; i++) {
+        let currentList = shoppingListArray[i]
+        
+        addListItem(currentList)
+    }
+})
+
+
 addButton.addEventListener('click', () => {
     let inputValue = inputField.value
 
